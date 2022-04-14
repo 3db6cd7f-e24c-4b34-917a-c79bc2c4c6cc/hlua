78251988f3f145455883fa417df05411e48038b1
Wildbook <book.wille@gmail.com>
2022-04-14 08:42:45 +0200

    Regenerate bindings with newer bindgen

--- lua52-sys/bindgen.sh
+++ lua52-sys/bindgen.sh
@@ -20,11 +20,11 @@ bindgen -o src/ffi.rs \
  ffi.h -- -I lua/src
 
 sed -i -e 's/pub fn \(luaJIT_[^\(]*\)/\/\/\/ <https:\/\/luajit.org\/ext_c_api.html> \n pub fn \1/' src/ffi.rs
-sed -i -e 's/pub fn \(lua_[^\(]*\)/\/\/\/ <https\:\/\/www.lua.org\/manual\/5.1\/manual.html#\1> \n pub fn \1/' src/ffi.rs
-sed -i -e 's/pub fn \(luaL_[^\(]*\)/\/\/\/ <https\:\/\/www.lua.org\/manual\/5.1\/manual.html#\1> \n pub fn \1/' src/ffi.rs
-sed -i -e 's/pub fn \(luaopen[^\(]*\)/\/\/\/ <https\:\/\/www.lua.org\/manual\/5.1\/manual.html#\1> \n pub fn \1/' src/ffi.rs
-sed -i -e 's/pub type \(lua_[^\=]*\)/\/\/\/ <https\:\/\/www.lua.org\/manual\/5.1\/manual.html#\1> \n pub type \1/' src/ffi.rs
-sed -i -e 's/pub struct \(lua_[^\{]*\)/\/\/\/ <https\:\/\/www.lua.org\/manual\/5.1\/manual.html#\1> \n pub struct \1/' src/ffi.rs
-sed -i -e 's/pub struct \(luaL_[^\{]*\)/\/\/\/ <https\:\/\/www.lua.org\/manual\/5.1\/manual.html#\1> \n pub struct \1/' src/ffi.rs
+sed -i -e 's/pub fn \(lua_[^ \(]*\)/\/\/\/ <https\:\/\/www.lua.org\/manual\/5.2\/manual.html#\1> \n pub fn \1/' src/ffi.rs
+sed -i -e 's/pub fn \(luaL_[^ \(]*\)/\/\/\/ <https\:\/\/www.lua.org\/manual\/5.2\/manual.html#\1> \n pub fn \1/' src/ffi.rs
+sed -i -e 's/pub fn \(luaopen[^ \(]*\)/\/\/\/ <https\:\/\/www.lua.org\/manual\/5.2\/manual.html#\1> \n pub fn \1/' src/ffi.rs
+sed -i -e 's/pub type \(lua_[^ \=]*\)/\/\/\/ <https\:\/\/www.lua.org\/manual\/5.2\/manual.html#\1> \n pub type \1/' src/ffi.rs
+sed -i -e 's/pub struct \(lua_[^ \{]*\)/\/\/\/ <https\:\/\/www.lua.org\/manual\/5.2\/manual.html#\1> \n pub struct \1/' src/ffi.rs
+sed -i -e 's/pub struct \(luaL_[^ \{]*\)/\/\/\/ <https\:\/\/www.lua.org\/manual\/5.2\/manual.html#\1> \n pub struct \1/' src/ffi.rs
 
-cargo +stable fmt
+cargo fmt
